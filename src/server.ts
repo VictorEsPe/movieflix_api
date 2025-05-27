82ed5c14dd83adcc1193f06725dfcd63f347841e
Victor Estevam Pereira <victorestevamp@gmail.com>
2025-05-26 21:09:49 -0300

    adicionada rota de cadastro

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,40 @@ const app = express();
 const prisma = new PrismaClient();
 const port = 3000;
 
-app.get("/movies", async (req, res) => {
+// o underline indica que o parâmetro req não é necessário no endpoint
+app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
-    orderBy:{
-      title: "asc"
+    orderBy: {
+      title: "asc",
     },
-    include:{
+    include: {
       genres: true,
-      languages: true
-    }
+      languages: true,
+    },
   });
   res.json(movies);
 });
 
-app.listen(port, () => console.log(`Servidor online em http://localhost:${port}`));
+// prepara o servidor para receber dados em json
+app.use(express.json());
+
+app.post("/movies", async (req, res) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  await prisma.movie.create({
+    data: {
+      // o id do filme é dispensável pois o banco de dados foi configurado para auto incrementar o id
+      title,
+      genre_id,
+      language_id,
+      oscar_count,
+      release_date: new Date(release_date),
+    },
+  });
+
+  res.status(201).send();
+});
+
+app.listen(port, () =>
+  console.log(`Servidor online em http://localhost:${port}`)
+);
