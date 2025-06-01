58d724b24cdfeafe9550ef69471f14d05e90d237
Victor Estevam Pereira <victorestevamp@gmail.com>
2025-06-01 19:30:08 -0300

    Adicionado deletar filmes

--- src/server.ts
+++ src/server.ts
@@ -89,6 +89,28 @@ app.put("/movies/:id", async (req, res) => {
   res.status(200).send();
 });
 
+app.delete("/movies/:id", async (req, res) => {
+  const id =  Number(req.params.id);
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: {id}
+    });
+
+    if(!movie) {
+      res.status(404).send({message: "Filme não encontrado"});
+    }
+
+    await prisma.movie.delete({where: {id}});
+  } catch (error) {
+    console.error(error);
+    
+    res.status(500).send({message: "Falha ao remover o registro"});
+  }
+
+  res.status(200).send();
+});
+
 app.listen(port, () =>
   console.log(`Servidor online em http://localhost:${port}`)
 );
